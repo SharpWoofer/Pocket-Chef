6e3a7f839980da67b01d195903f5ec1715095e14
timtaifung <nyantimothy@gmail.com>
2024-04-09 01:50:35 +0800

    measurement page UI/UX improvement

--- client/src/routes/calculator/calculator.jsx
+++ client/src/routes/calculator/calculator.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import './CalculatorPage.css'; 
+import './CalculatorPage.css';
+import {Box, Grid, Paper, Stack, Typography} from "@mui/material";
+import CalorieGraph from "../calorietracker/calorieGraph.jsx";
 
 const CalculatorPage = () => {
   const [weight, setWeight] = useState('');
@@ -39,73 +41,185 @@ const CalculatorPage = () => {
   
 
   return (
-    <div className="calculator-container">
-      <h1>BMI Calculator</h1>
-      <div className="input-container">
-        <label htmlFor="weight">Weight (kg):</label>
-        <input
-        type="number"
-        id="weight"
-        className="input"
-        value={weight}
-        onChange={(e) => setWeight(e.target.value)} // handle weight input change
-      />
+      <Stack direction="row" sx={{
+          display:"flex",
+          justifyContent: "space-around",
+          height:"100vh"
+      }}>
+         <Grid sx={{
+             width: "55%",
+         }}>
+             <Box
+                 sx={{
+                     borderBottom: 2,
+                     borderColor: '#12365F',
+                     height: "40vh",
+                     width: "100%",
+                 }}>
+                 <Typography
+                     variant="h1" // Changed from 'header1' to 'h1' for correct variant usage
+                     sx={{
+                         color: '#12365F',
+                         textAlign: 'start',
+                         fontSize: "9vh",
+                         paddingLeft: "0.2em",
+                         fontWeight: "bolder",
+                         letterSpacing: "2px",
+                         paddingBottom: "0.25em", // Adjust this value as needed for proper underline spacing
+                     }}>
+                     Calculate Your Measurements
+                 </Typography>
 
-      </div>
-      <div className="input-container">
-        <label htmlFor="height">Height (cm):</label>
-        <input
-        type="number"
-        id="height"
-        className="input"
-        value={height}
-        onChange={(e) => setHeight(e.target.value)} // handle height input change
-      />
-      </div>
-      <button className="button" onClick={calculateBMI}>Calculate BMI</button>
-      {bmi && (
-        <div className="result-container">
-          <h2>Your BMI is: {bmi}</h2>
-        </div>
-      )}
-      <div className="measurement-converter-container">
-      <h1>Measurement Converter</h1>
-      <div className="input-container">
-        <label htmlFor="value">Value:</label>
-        <input
-          type="number"
-          id="value"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-          className="input"
-        />
-      </div>
-      <div className="input-container">
-        <label htmlFor="fromUnit">From:</label>
-        <select id="fromUnit" value={fromUnit} onChange={(e) => setFromUnit(e.target.value)} className="input">
-          <option value="grams">Grams</option>
-          <option value="ounces">Ounces</option>
-          <option value="tablespoons">Tablespoons</option>
-          {/* Add more options as needed here*/}
-        </select>
-      </div>
-      <div className="input-container">
-        <label htmlFor="toUnit">To:</label>
-        <select id="toUnit" value={toUnit} onChange={(e) => setToUnit(e.target.value)} className="input">
-          <option value="grams">Grams</option>
-          <option value="ounces">Ounces</option>
-          <option value="tablespoons">Tablespoons</option>
-          {/* Add more options as needed here*/}
-        </select>
-      </div>
-      <button className="button" onClick={convertMeasurement}>Convert</button>
-      {convertedValue && (
-        <div className="result-container">
-          <h2>Converted Value: {convertedValue} {toUnit}</h2>
-        </div>
-      )}
-    </div>
-    </div>
+                 <Typography style={{ color: '#1236F', textAlign: 'start', fontSize:"1.8vh", paddingLeft:"0.2em", letterSpacing:"2px", marginLeft:"1em", marginTop:"1em", marginBottom:"2em"}}>
+                     Embark on a seamless journey of health and precision with our all-in-one measurement converter and BMI calculator! Whether you're fine-tuning your recipes by converting pounds to kilograms or teaspoons, or keeping a vigilant eye on your well-being by tracking your BMI, our intuitive tools are designed to empower your lifestyle choices. Embrace accuracy and wellness, all with just a few clicks - it's never been easier to stay on top of your health and culinary game!                 </Typography>
+             </Box>
+             <Box style={{display:"flex", justifyContent:"space-around"}}>
+                 <div className="calculator-container">
+                     <h1>BMI Calculator</h1>
+                     <div className="input-container">
+                         <label htmlFor="weight">Weight (kg):</label>
+                         <input
+                             type="number"
+                             id="weight"
+                             className="input"
+                             value={weight}
+                             onChange={(e) => setWeight(e.target.value)} // handle weight input change
+                         />
+
+                     </div>
+                     <div className="input-container">
+                         <label htmlFor="height">Height (cm):</label>
+                         <input
+                             type="number"
+                             id="height"
+                             className="input"
+                             value={height}
+                             onChange={(e) => setHeight(e.target.value)} // handle height input change
+                         />
+                     </div>
+                     <button className="button" onClick={calculateBMI}>Calculate BMI</button>
+                     {bmi && (
+                         <div className="result-container">
+                             <h2>Your BMI is: {bmi}</h2>
+                         </div>
+                     )}
+                 </div>
+                 <div className="calculator-container">
+                     <div className="measurement-converter-container">
+                         <h1>Measurement Converter</h1>
+                         <div className="input-container">
+                             <label htmlFor="value">Value:</label>
+                             <input
+                                 type="number"
+                                 id="value"
+                                 value={value}
+                                 onChange={(e) => setValue(e.target.value)}
+                                 className="input"
+                             />
+                         </div>
+                         <div className="input-container">
+                             <label htmlFor="fromUnit">From:</label>
+                             <select id="fromUnit" value={fromUnit} onChange={(e) => setFromUnit(e.target.value)} className="input">
+                                 <option value="grams">Grams</option>
+                                 <option value="ounces">Ounces</option>
+                                 <option value="tablespoons">Tablespoons</option>
+                                 {/* Add more options as needed here*/}
+                             </select>
+                         </div>
+                         <div className="input-container">
+                             <label htmlFor="toUnit">To:</label>
+                             <select id="toUnit" value={toUnit} onChange={(e) => setToUnit(e.target.value)} className="input">
+                                 <option value="grams">Grams</option>
+                                 <option value="ounces">Ounces</option>
+                                 <option value="tablespoons">Tablespoons</option>
+                             </select>
+                         </div>
+                         <button className="button" onClick={convertMeasurement}>Convert</button>
+                         {convertedValue && (
+                             <div className="result-container">
+                                 <h2>Converted Value: {convertedValue} {toUnit}</h2>
+                             </div>
+                         )}
+                     </div>
+                 </div>
+             </Box>
+         </Grid>
+          <Grid sx={{
+              width: "40%",
+          }} style={{display:"flex", justifyContent:"end"}}>
+                  <img src="./barista.png" alt="eating" style={{
+                      width: "100%",
+                      height: "80%",
+                  }} />
+          </Grid>
+      </Stack>
+    // <div className="calculator-container">
+    //   <h1>BMI Calculator</h1>
+    //   <div className="input-container">
+    //     <label htmlFor="weight">Weight (kg):</label>
+    //     <input
+    //     type="number"
+    //     id="weight"
+    //     className="input"
+    //     value={weight}
+    //     onChange={(e) => setWeight(e.target.value)} // handle weight input change
+    //   />
+    //
+    //   </div>
+    //   <div className="input-container">
+    //     <label htmlFor="height">Height (cm):</label>
+    //     <input
+    //     type="number"
+    //     id="height"
+    //     className="input"
+    //     value={height}
+    //     onChange={(e) => setHeight(e.target.value)} // handle height input change
+    //   />
+    //   </div>
+    //   <button className="button" onClick={calculateBMI}>Calculate BMI</button>
+    //   {bmi && (
+    //     <div className="result-container">
+    //       <h2>Your BMI is: {bmi}</h2>
+    //     </div>
+    //   )}
+    //   <div className="measurement-converter-container">
+    //   <h1>Measurement Converter</h1>
+    //   <div className="input-container">
+    //     <label htmlFor="value">Value:</label>
+    //     <input
+    //       type="number"
+    //       id="value"
+    //       value={value}
+    //       onChange={(e) => setValue(e.target.value)}
+    //       className="input"
+    //     />
+    //   </div>
+    //   <div className="input-container">
+    //     <label htmlFor="fromUnit">From:</label>
+    //     <select id="fromUnit" value={fromUnit} onChange={(e) => setFromUnit(e.target.value)} className="input">
+    //       <option value="grams">Grams</option>
+    //       <option value="ounces">Ounces</option>
+    //       <option value="tablespoons">Tablespoons</option>
+    //       {/* Add more options as needed here*/}
+    //     </select>
+    //   </div>
+    //   <div className="input-container">
+    //     <label htmlFor="toUnit">To:</label>
+    //     <select id="toUnit" value={toUnit} onChange={(e) => setToUnit(e.target.value)} className="input">
+    //       <option value="grams">Grams</option>
+    //       <option value="ounces">Ounces</option>
+    //       <option value="tablespoons">Tablespoons</option>
+    //       {/* Add more options as needed here*/}
+    //     </select>
+    //   </div>
+    //   <button className="button" onClick={convertMeasurement}>Convert</button>
+    //   {convertedValue && (
+    //     <div className="result-container">
+    //       <h2>Converted Value: {convertedValue} {toUnit}</h2>
+    //     </div>
+    //   )}
+    // </div>
+    // </div>
   );
 };
 
