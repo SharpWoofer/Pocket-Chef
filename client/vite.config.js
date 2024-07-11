5a40be9438c9092d5152ea0689a228f3baf704a9
SharpWoofer <cai_hongqi@yahoo.com>
2024-07-12 00:19:22 +0800

    changed base of vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-})
+  base: '/Pocket-Chef/',
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom']
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
+});
